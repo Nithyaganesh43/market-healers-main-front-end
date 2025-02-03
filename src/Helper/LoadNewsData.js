44481d3a2692dfc7d966ce73aba06994fe731602
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-02-03 10:02:14 +0530

    yes

--- src/Helper/LoadNewsData.js
+++ src/Helper/LoadNewsData.js
@@ -1,38 +1,30 @@
-const getCurrentDateObj = (simulatedDate = null) => {
-  const date =
-    simulatedDate ||
-    new Date().toLocaleString('en-GB', {
-      timeZone: 'Asia/Kolkata',
-      hour12: false,
-    });
-  const [datePart, timePart] = date.split(', ');
-  return { date: datePart.replace(/\//g, '-'), time: timePart };
-};
-
-function isFourHoursApart(dt1, dt2) {
-  const formatDate = (d) => {
-    let [day, month, year] = d.date.split('-');
-    return `${year}-${month}-${day} ${d.time}`;
-  };
-
-  let t1 = new Date(formatDate(dt1));
-  let t2 = new Date(formatDate(dt2));
-
-  return Math.abs(t2 - t1) >= 4 * 60 * 60 * 1000;
+function isThreeHoursOlder(lastUpdatedStr) {
+  const lastUpdated = new Date(
+    lastUpdatedStr.split('-').reverse().join('-') +
+      'T' +
+      lastUpdatedStr.split(' ')[1]
+  );
+  const now = new Date();
+  const diff = now - lastUpdated;
+  return diff >= 6 * 60 * 60 * 1000;
 }
-
 
 export async function getNewsData(setMainNewsData, setloading, setNewsData) {
   let news = localStorage.getItem('news');
   if (news) {
     try {
       news = JSON.parse(news);
-      if (!isFourHoursApart(news.data.lastUpdated, getCurrentDateObj())) {
-      console.log("less then 4h")
+      if (
+        news &&
+        !isThreeHoursOlder(
+          news.data.lastUpdated.date + ' ' + news.data.lastUpdated.time
+        )
+      ) {
+        console.log("not older")
+        setloading(false);
+        console.log(news.data.lastUpdated);
         setMainNewsData(Object.values(news.data.data));
         setNewsData(Object.values(news.data.data));
-        console.log('Loader' + Object.values(news.data.data));
-        setloading(false);
         return;
       }
     } catch (e) {
@@ -42,18 +34,17 @@ export async function getNewsData(setMainNewsData, setloading, setNewsData) {
 
   try {
     setloading(true);
-    console.log('fetched');
+    console.log("fetch")
     let data = await fetch(
-      'https://server.markethealers.com/MarketHealers/getNewsData',
-      {
-        credentials: 'include',
-      }
+      'https://server.markethealers.com/MarketHealers/getNewsData'
     );
     if (!data.ok) {
       throw new Error('Failed to fetch news data');
     }
     data = await data.json();
     localStorage.setItem('news', JSON.stringify(data));
+    setMainNewsData(Object.values(data.data.data));
+    setNewsData(Object.values(data.data.data));
 
     setloading(false);
   } catch (error) {
