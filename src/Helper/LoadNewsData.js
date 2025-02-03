c0fdcd655282e634513dad60dc09c7b3b23456e8
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-02-03 09:44:14 +0530

    yes

--- src/Helper/LoadNewsData.js
+++ src/Helper/LoadNewsData.js
@@ -1,30 +1,34 @@
 const getCurrentDateObj = (simulatedDate = null) => {
-  const date = simulatedDate
-    ? simulatedDate
-    : new Date().toLocaleString('en-US', {
-        timeZone: 'America/New_York',
-        hour12: false,
-      });
+  const date =
+    simulatedDate ||
+    new Date().toLocaleString('en-GB', {
+      timeZone: 'Asia/Kolkata',
+      hour12: false,
+    });
   const [datePart, timePart] = date.split(', ');
   return { date: datePart.replace(/\//g, '-'), time: timePart };
 };
 
-
 function isFourHoursApart(dt1, dt2) {
-  let t1 = new Date(`${dt1.date} ${dt1.time}`);
-  let t2 = new Date(`${dt2.date} ${dt2.time}`);
+  const formatDate = (d) => {
+    let [day, month, year] = d.date.split('-');
+    return `${year}-${month}-${day} ${d.time}`;
+  };
+
+  let t1 = new Date(formatDate(dt1));
+  let t2 = new Date(formatDate(dt2));
+
   return Math.abs(t2 - t1) >= 4 * 60 * 60 * 1000;
 }
- 
+
+
 export async function getNewsData(setMainNewsData, setloading, setNewsData) {
   let news = localStorage.getItem('news');
   if (news) {
     try {
-      news = JSON.parse(news); 
-      if ( 
-        !isFourHoursApart(news.data.lastUpdated, getCurrentDateObj())
-      ) { 
-        console.log("local news ea ok")
+      news = JSON.parse(news);
+      if (!isFourHoursApart(news.data.lastUpdated, getCurrentDateObj())) {
+      console.log("less then 4h")
         setMainNewsData(Object.values(news.data.data));
         setNewsData(Object.values(news.data.data));
         setloading(false);
@@ -40,15 +44,15 @@ export async function getNewsData(setMainNewsData, setloading, setNewsData) {
     console.log('fetched');
     let data = await fetch(
       'https://server.markethealers.com/MarketHealers/getNewsData',
-      { 
-        credentials: 'include',  
+      {
+        credentials: 'include',
       }
     );
     if (!data.ok) {
       throw new Error('Failed to fetch news data');
     }
     data = await data.json();
-    localStorage.setItem('news', JSON.stringify(data)); 
+    localStorage.setItem('news', JSON.stringify(data));
 
     setloading(false);
   } catch (error) {
