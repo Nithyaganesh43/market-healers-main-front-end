0628ef4de77fe3f311a96837a12966aacbb1411c
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-03-05 22:17:15 +0530

    f

--- src/Helper/MarketPageHelper.js
+++ src/Helper/MarketPageHelper.js
@@ -44,9 +44,9 @@ async function fetchMarketValueData(setIsLoading, setMarketValueData) {
     const response = await fetch(
       'https://server.markethealers.com/markethealers/getMarketdata'
     );
-    console.log(response);
     if (!response.ok) throw new Error('Failed to fetch market data');
     const data = await response.json();
+    console.log(data);
     setMarketValueData(data?.data || {});
 
     return timeDiff(data.data.lastUpdated);
